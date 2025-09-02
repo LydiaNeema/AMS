ea606a7eaa2c4f58cd73c23fa7448075d7a34f7e
Lydia Neema <lydianeema25@gmail.com>
2025-09-02 09:56:26 +0300

    created the navbar component

---
+++ ams/components/navbar.jsx
@@ -0,0 +1,15 @@
+
+export default function Navbar(){
+    return(
+        <div>
+            <nav>
+                <ul>
+                    <li><a href = "#home">HOME</a></li>
+                    <li><a href = "#about">ABOUT</a></li>
+                    <li><a href = "#services">SERVICES</a></li>
+                    <li><a href = "#contact">CONTACT</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
